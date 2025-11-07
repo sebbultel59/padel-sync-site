d6d58cec55b032cf994d1e2fbd1afa216460bdc6
Sebastien Bultel <sebbultel@macbookair-1.home>
2025-11-07 17:37:35 +0100

    Replace profile icon with help icon in header to start tutorial

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -7,7 +7,7 @@ import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Modal, Pressable, Text, useWindowDimensions, View } from 'react-native';
 import { CopilotStep } from 'react-native-copilot';
 import 'react-native-gesture-handler';
-import { CopilotTutorialProvider } from '../../components/CopilotTutorial';
+import { CopilotTutorialProvider, getGlobalCopilotStart } from '../../components/CopilotTutorial';
 import { copilotSteps } from '../../lib/copilotSteps';
 import { supabase } from '../../lib/supabase';
 
